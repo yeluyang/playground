b9a98a31ce843c8d09f8d8cc7d4548b6d386b59a
yeluyang <ylycpg@gmail.com>
2019-12-16 13:09:29 +0800

    code: outline API of kvs

---
+++ rust/pingcap/talent-plan/kvs/src/lib.rs
@@ -0,0 +1,16 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        unimplemented!();
+    }
+    pub fn set(&mut self, _: String, _: String) {
+        unimplemented!();
+    }
+    pub fn get(&self, _: String) -> Option<String> {
+        unimplemented!();
+    }
+    pub fn remove(&mut self, _: String) {
+        unimplemented!();
+    }
+}
