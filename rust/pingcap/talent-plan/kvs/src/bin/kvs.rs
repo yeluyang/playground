02dcacd0e5e6f109ca7d948496c4851296aa026c
yeluyang <ylycpg@gmail.com>
2019-12-16 13:09:36 +0800

    code: add cli-interface for kvs

--- rust/pingcap/talent-plan/kvs/src/bin/kvs.rs
+++ rust/pingcap/talent-plan/kvs/src/bin/kvs.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .get_matches();
 }
