18a1634c62d5c8463c46ca6bcc1b7b4ecfb182b5
yeluyang <ylycpg@gmail.com>
2019-12-24 17:46:33 +0800

    code: implement rust/city-pop

--- rust/city-pop/src/lib.rs
+++ rust/city-pop/src/lib.rs
@@ -1,4 +1,36 @@
+extern crate csv;
+extern crate serde;
+
+use csv::Reader;
+use serde::Deserialize;
+
+use std::fs::File;
+
 #[cfg(test)]
 mod tests;
 
 pub mod error;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Row {
+    country: String,
+    city: String,
+    accent_city: String,
+    region: String,
+    population: Option<u64>,
+    latitude: Option<f64>,
+    longitude: Option<f64>,
+}
+
+pub fn find_city_in_csv(city_name: String, file_path: String) -> Option<Row> {
+    let fd = File::open(file_path).unwrap();
+    let mut rdr = Reader::from_reader(fd);
+    for row in rdr.deserialize() {
+        let row: Row = row.unwrap();
+        if row.city == city_name {
+            return Some(row);
+        }
+    }
+    None
+}
--- rust/city-pop/src/main.rs
+++ rust/city-pop/src/main.rs
@@ -1,3 +1,28 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
+use std::process;
+
 use city_pop::*;
 
-fn main() {}
+fn main() {
+    let matches = App::new(clap::crate_name!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .args(&[
+            Arg::with_name("DATA_PATH").required(true),
+            Arg::with_name("CITY_NAME").required(true),
+        ])
+        .get_matches();
+    if matches.args.is_empty() {
+        println!("{}", matches.usage());
+        process::exit(1);
+    }
+    let result = find_city_in_csv(
+        matches.value_of("CITY_NAME").unwrap().to_owned(),
+        matches.value_of("DATA_PATH").unwrap().to_owned(),
+    )
+    .unwrap();
+    println!("{:?}", result);
+}
