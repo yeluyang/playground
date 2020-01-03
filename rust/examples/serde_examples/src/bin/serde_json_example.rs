9e84a07a153f80e11b5a68ceb7dd7a534b9a7584
yeluyang <ylycpg@gmail.com>
2020-01-03 17:12:05 +0800

    code: add serde ron example in examples/serde

--- rust/examples/serde_examples/src/bin/serde_json_example.rs
+++ rust/examples/serde_examples/src/bin/serde_json_example.rs
@@ -7,22 +7,31 @@ use serde_examples::Config;
 fn main() {
     let cfg = Config::default();
 
+    // ser to json string
     let c_json_str = serde_json::to_string_pretty(&cfg).unwrap();
     println!("json string = {}", c_json_str);
+
+    // deser from json string
     let c_from_json_str: Config = serde_json::from_str(&c_json_str).unwrap();
     println!("from json string = {}", c_from_json_str.to_string());
     assert_eq!(cfg, c_from_json_str);
 
+    // ser to file.json
     File::create("tmp/tmp.json")
         .unwrap()
         .write_all(c_json_str.as_bytes())
         .unwrap();
+
+    // deser from file.json
     let c_from_json_file: Config =
         serde_json::from_reader(File::open("tmp/tmp.json").unwrap()).unwrap();
     println!("from json file = {}", c_from_json_file.to_string());
     assert_eq!(cfg, c_from_json_file);
 
+    // convert json string to bytes
     let c_json_bytes = Vec::from(c_json_str.as_bytes());
+
+    // deser from bytes
     let c_from_json_bytes: Config = serde_json::from_slice(c_json_bytes.as_slice()).unwrap();
     println!("from json bytes = {}", c_from_json_bytes.to_string());
     assert_eq!(cfg, c_from_json_bytes);
---
+++ rust/examples/serde_examples/src/bin/serde_ron_example.rs
@@ -0,0 +1,39 @@
+use std::{fs::File, io::Write};
+
+extern crate ron;
+
+use ron::{de, ser};
+
+use serde_examples::Config;
+
+fn main() {
+    let cfg = Config::default();
+
+    // ser to ron string
+    let c_ron_str = ser::to_string(&cfg).unwrap();
+    println!("ron string = {}", c_ron_str);
+
+    // deser from ron string
+    let c_from_ron_str: Config = de::from_str(&c_ron_str).unwrap();
+    println!("from ron string = {}", c_from_ron_str);
+    assert_eq!(cfg, c_from_ron_str);
+
+    // ser to file.ron
+    File::create("tmp/tmp.ron")
+        .unwrap()
+        .write_all(c_ron_str.as_bytes())
+        .unwrap();
+
+    // deser from file.ron
+    let c_from_ron_file: Config = de::from_reader(File::open("tmp/tmp.ron").unwrap()).unwrap();
+    println!("from ron file = {}", c_from_ron_file.to_string());
+    assert_eq!(cfg, c_from_ron_file);
+
+    // convert ron string to bytes
+    let c_ron_bytes = Vec::from(c_ron_str.as_bytes());
+
+    // deser from bytes
+    let c_from_ron_bytes: Config = de::from_bytes(c_ron_bytes.as_slice()).unwrap();
+    println!("from ron bytes = {}", c_from_ron_bytes);
+    assert_eq!(cfg, c_from_ron_bytes);
+}
