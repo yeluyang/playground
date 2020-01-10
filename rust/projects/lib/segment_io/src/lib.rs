02a356ecdcd3b8065e51cdeda2311a0b34b437d6
yeluyang <ylycpg@gmail.com>
2020-01-10 13:17:05 +0800

    code: impl rust/projects/lib/segment_io

--- rust/projects/lib/segment_io/src/lib.rs
+++ rust/projects/lib/segment_io/src/lib.rs
@@ -1,2 +1,116 @@
+use std::{
+    fs::File,
+    io::{self, Cursor, Read, Seek, SeekFrom, Write},
+    mem,
+    path::Path,
+};
+
+extern crate byteorder;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+
+extern crate buff_io;
+use buff_io::Buffer;
+
 #[cfg(test)]
 mod tests;
+
+type Endian = LittleEndian;
+
+const HEADER_SIZE: usize = mem::size_of::<Header>();
+#[derive(Default, Debug, PartialEq)]
+struct Header {
+    length: u128,
+    seq_id: u128,
+    total: u128,
+}
+
+impl Header {
+    pub fn new(data_len: usize) -> Self {
+        Header {
+            length: data_len as u128,
+            seq_id: 0,
+            total: 0,
+        }
+    }
+
+    pub fn from(bs: &[u8]) -> Self {
+        assert_eq!(bs.len(), HEADER_SIZE);
+
+        let mut h = Header::default();
+
+        let mut rdr = Cursor::new(Vec::from(bs));
+
+        h.length = rdr.read_u128::<Endian>().unwrap();
+        h.seq_id = rdr.read_u128::<Endian>().unwrap();
+        h.total = rdr.read_u128::<Endian>().unwrap();
+
+        h
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut wtr = Vec::new();
+
+        wtr.write_u128::<Endian>(self.length).unwrap();
+        wtr.write_u128::<Endian>(self.seq_id).unwrap();
+        wtr.write_u128::<Endian>(self.total).unwrap();
+
+        if wtr.len() != HEADER_SIZE {
+            panic!(
+                "length of Header's bytes={}, expected={}",
+                wtr.len(),
+                HEADER_SIZE
+            );
+        }
+
+        wtr
+    }
+}
+
+pub struct SegmentFile {
+    buff: Buffer<File>,
+}
+
+impl SegmentFile {
+    pub fn new(fd: File) -> io::Result<SegmentFile> {
+        let mut buff = Buffer::new(fd)?;
+        buff.seek(SeekFrom::Start(0))?;
+        Ok(SegmentFile { buff })
+    }
+
+    pub fn open<P: AsRef<Path>>(path: P) -> io::Result<SegmentFile> {
+        SegmentFile::new(File::open(path)?)
+    }
+
+    pub fn create<P: AsRef<Path>>(path: P) -> io::Result<SegmentFile> {
+        SegmentFile::new(File::create(path)?)
+    }
+}
+
+impl Read for SegmentFile {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let h_buf: &mut [u8] = &mut [0u8; HEADER_SIZE];
+        self.buff.read_exact(h_buf)?;
+        let h = Header::from(h_buf);
+
+        if buf.len() < h.length as usize {
+            panic!("length of buffer={}, expected={}", buf.len(), h.length);
+        }
+
+        let mut data = vec![0u8; h.length as usize];
+        let len = self.buff.read(data.as_mut_slice())?;
+        buf[..len].clone_from_slice(&data[..len]);
+        Ok(len)
+    }
+}
+
+impl Write for SegmentFile {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let h = Header::new(buf.len());
+        self.buff.write_all(h.to_bytes().as_slice())?;
+        let len = self.buff.write(buf)?;
+        Ok(len)
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        self.buff.flush()
+    }
+}
---
+++ rust/projects/lib/segment_io/src/tests.rs
@@ -0,0 +1,71 @@
+use std::{
+    fs,
+    io::{Read, Write},
+    path::Path,
+};
+
+extern crate serde;
+use serde::{Deserialize, Serialize};
+
+extern crate serde_json;
+
+use crate::{Header, SegmentFile, HEADER_SIZE};
+
+#[test]
+fn test_header() {
+    let h1 = Header {
+        length: 1024,
+        seq_id: 512,
+        total: 256,
+    };
+    assert_eq!(h1.to_bytes().len(), HEADER_SIZE);
+
+    let h2 = Header::from(&h1.to_bytes());
+    assert_eq!(h2, h1);
+}
+
+#[test]
+fn test_segment_file_io() {
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct Case {
+        id: usize,
+        data: Vec<String>,
+    }
+    let cases = [
+        Case {
+            id: 0,
+            data: vec![],
+        },
+        Case {
+            id: 1,
+            data: vec!["hello".to_owned(), "world".to_owned()],
+        },
+        Case {
+            id: 2,
+            data: vec!["end".to_owned()],
+        },
+    ];
+
+    let tmp_dir = Path::new("tmp");
+    if !tmp_dir.exists() {
+        fs::create_dir(tmp_dir).unwrap();
+    }
+
+    {
+        let mut s_file = SegmentFile::create("tmp/tmp.txt").unwrap();
+        for case in &cases {
+            let bs = serde_json::to_vec(case).unwrap();
+            assert_eq!(s_file.write(bs.as_slice()).unwrap(), bs.len());
+        }
+    }
+
+    let mut s_file = SegmentFile::open("tmp/tmp.txt").unwrap();
+    let mut buf = vec![0u8; 2048];
+    for case in &cases {
+        let bs = serde_json::to_vec(case).unwrap();
+        assert_eq!(s_file.read(buf.as_mut_slice()).unwrap(), bs.len());
+        assert_eq!(&buf[..bs.len()], bs.as_slice());
+        let c: Case = serde_json::from_slice(&buf[..bs.len()]).unwrap();
+        assert_eq!(&c, case);
+    }
+}
