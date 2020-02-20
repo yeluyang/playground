19087abda86b1b1b3885c9bb731e3edf5e50c0c5
yeluyang <ylycpg@gmail.com>
2020-02-20 11:46:20 +0800

    code[dist/pingcap]: add log in kvs/bin

--- distributed/pingcap/talent-plan/kvs/src/bin/kvs-client.rs
+++ distributed/pingcap/talent-plan/kvs/src/bin/kvs-client.rs
@@ -1,7 +1,12 @@
 extern crate clap;
-
 use clap::{App, AppSettings, Arg, SubCommand};
 
+#[macro_use]
+extern crate log;
+use log::LevelFilter;
+
+extern crate env_logger;
+use env_logger::Env;
 use kvs::{self, KvStore, Result};
 
 fn main() -> Result<()> {
@@ -12,10 +17,18 @@ fn main() -> Result<()> {
             AppSettings::ArgRequiredElseHelp,
             AppSettings::SubcommandRequired,
         ])
-        .args(&[Arg::with_name("verbose")
-            .short("v")
-            .takes_value(false)
-            .multiple(true)])
+        .args(&[
+            Arg::with_name("verbose")
+                .short("v")
+                .takes_value(false)
+                .multiple(true)
+                .conflicts_with("quiet"),
+            Arg::with_name("quiet")
+                .long("quiet")
+                .short("q")
+                .takes_value(false)
+                .conflicts_with("verbose"),
+        ])
         .subcommands(vec![
             SubCommand::with_name("set").args(&[
                 Arg::with_name("KEY").required(true).takes_value(true),
@@ -28,23 +41,48 @@ fn main() -> Result<()> {
         ])
         .get_matches();
 
+    let log_level = if matches.is_present("quiet") {
+        LevelFilter::Off
+    } else {
+        match matches.occurrences_of("verbose") {
+            0 => LevelFilter::Error,
+            1 => LevelFilter::Info,
+            2 => LevelFilter::Debug,
+            _ => LevelFilter::Trace,
+        }
+    };
+    env_logger::init_from_env(Env::default().default_filter_or(log_level.to_string()));
+
+    info!("client start");
+
     let mut kv_store = KvStore::open(std::env::current_dir()?)?;
+    info!("loaded data into KV store");
+
     match matches.subcommand() {
-        ("set", Some(m)) => kv_store.set(
-            m.value_of("KEY").unwrap().to_owned(),
-            m.value_of("VALUE").unwrap().to_owned(),
-        ),
-        ("get", Some(m)) => kv_store
-            .get(m.value_of("KEY").unwrap().to_owned())
-            .map_err(|err| {
-                eprintln!("{}", err);
-                err
-            })
-            .map(|opt| match opt {
-                Some(val) => println!("{}", val),
-                None => println!("Key not found"),
-            }),
-        ("rm", Some(m)) => kv_store.remove(m.value_of("KEY").unwrap().to_owned()),
+        ("set", Some(m)) => {
+            info!("setting key-value");
+            kv_store.set(
+                m.value_of("KEY").unwrap().to_owned(),
+                m.value_of("VALUE").unwrap().to_owned(),
+            )
+        }
+        ("get", Some(m)) => {
+            info!("getting value by key");
+            kv_store
+                .get(m.value_of("KEY").unwrap().to_owned())
+                .map_err(|err| {
+                    eprintln!("{}", err);
+                    err
+                })
+                .map(|opt| match opt {
+                    Some(val) => println!("{}", val),
+                    None => println!("Key not found"),
+                })
+        }
+        ("rm", Some(m)) => {
+            info!("removing key-value");
+            kv_store.remove(m.value_of("KEY").unwrap().to_owned())
+        }
         _ => unreachable!(),
     }
 }
--- distributed/pingcap/talent-plan/kvs/src/bin/kvs-server.rs
+++ distributed/pingcap/talent-plan/kvs/src/bin/kvs-server.rs
@@ -1,11 +1,30 @@
 extern crate clap;
 use clap::{App, AppSettings, Arg};
 
+#[macro_use]
+extern crate log;
+use log::LevelFilter;
+
+extern crate env_logger;
+use env_logger::Env;
+
 fn main() {
     let matches = App::new("kvs-server")
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
         .settings(&[AppSettings::ArgRequiredElseHelp])
+        .args(&[
+            Arg::with_name("verbose")
+                .short("v")
+                .takes_value(false)
+                .multiple(true)
+                .conflicts_with("quiet"),
+            Arg::with_name("quiet")
+                .long("quiet")
+                .short("q")
+                .takes_value(false)
+                .conflicts_with("verbose"),
+        ])
         .args(&[
             Arg::with_name("IP:PORT")
                 .long("addr")
@@ -18,4 +37,18 @@ fn main() {
                 .takes_value(true),
         ])
         .get_matches();
+
+    let log_level = if matches.is_present("quiet") {
+        LevelFilter::Off
+    } else {
+        match matches.occurrences_of("verbose") {
+            0 => LevelFilter::Error,
+            1 => LevelFilter::Info,
+            2 => LevelFilter::Debug,
+            _ => LevelFilter::Trace,
+        }
+    };
+    env_logger::init_from_env(Env::default().default_filter_or(log_level.to_string()));
+
+    info!("server start");
 }
