d7796cb644445dab13f44c2af5220ef22786e185
yeluyang <ylycpg@gmail.com>
2020-03-10 16:09:13 +0800

    code[rust]: add dead-lock in examples/concurrence

--- rust/examples/concurrence_examples/src/bin/try_to_die_example.rs
+++ rust/examples/concurrence_examples/src/bin/try_to_die_example.rs
@@ -1,7 +1,49 @@
-/// TODO try to challenge safety of rust
+//! try to challenge safety of rust
 
-use std::thread;
+#![feature(is_sorted)]
+
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 fn main() {
-    thread::spawn(|| {});
+    let counter = Arc::new(Mutex::new(0usize));
+    let counter_a = counter.clone();
+    let counter_b = counter.clone();
+
+    let counts = Arc::new(Mutex::new(Vec::new() as Vec<usize>));
+    let shared_a = counts.clone();
+    let shared_b = counts.clone();
+
+    let (sender, receiver) = mpsc::channel::<String>();
+    let sender_a = sender.clone();
+    let sender_b = sender.clone();
+
+    // following code will cause Dead-Lock
+    // can package all related data in one mutex to avoid it
+
+    thread::spawn(move || loop {
+        let mut counts = shared_a.lock().unwrap();
+        sender_a.send("A hold counts, wait for counter".to_owned());
+        let mut counter = counter_a.lock().unwrap();
+        sender_a.send("A hold both counts and counter".to_owned());
+
+        *counter += 1;
+        counts.push(*counter);
+    });
+
+    thread::spawn(move || loop {
+        let mut counter = counter_b.lock().unwrap();
+        sender_b.send("B hold counter, wait for counts".to_owned());
+        let mut counts = shared_b.lock().unwrap();
+        sender_b.send("B hold both counts and counter".to_owned());
+
+        *counter += 1;
+        counts.push(*counter);
+    });
+
+    loop {
+        println!("{}", receiver.recv().unwrap());
+    }
 }
