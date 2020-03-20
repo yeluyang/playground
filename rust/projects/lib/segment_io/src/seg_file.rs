ba999fe39c8282b8135b45a27b2e73fe6b3f7845
yeluyang <ylycpg@gmail.com>
2020-03-20 11:44:52 +0800

    refactor[rust]: refactor segment_io

---
+++ rust/projects/lib/segment_io/src/seg_file.rs
@@ -0,0 +1,396 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, BufReader, BufWriter, Cursor, Read, Seek, SeekFrom, Write},
+    mem,
+    path::Path,
+};
+
+use crate::segment::{self, Segment};
+use crate::{Endian, ReadBytesExt, WriteBytesExt};
+
+#[derive(Debug, Default, PartialEq)]
+struct FileHeader {
+    header_bytes: u128,
+    payload_bytes: u128,
+}
+const FILE_HEADER_SIZE: usize = mem::size_of::<FileHeader>();
+
+impl FileHeader {
+    fn new(payload_bytes: u128) -> Self {
+        Self {
+            header_bytes: segment::SEGMENT_HEADER_SIZE as u128,
+            payload_bytes,
+        }
+    }
+    fn from(bs: &[u8]) -> io::Result<Self> {
+        assert_eq!(bs.len(), FILE_HEADER_SIZE);
+
+        let mut rdr = Cursor::new(Vec::from(bs));
+        let header_bytes = rdr.read_u128::<Endian>()?;
+        let payload_bytes = rdr.read_u128::<Endian>()?;
+
+        Ok(Self {
+            header_bytes,
+            payload_bytes,
+        })
+    }
+
+    fn to_bytes(&self) -> io::Result<Vec<u8>> {
+        let mut wtr = Vec::new();
+        wtr.write_u128::<Endian>(self.header_bytes)?;
+        wtr.write_u128::<Endian>(self.payload_bytes)?;
+
+        if wtr.len() != FILE_HEADER_SIZE {
+            panic!(
+                "length of Version's bytes={}, expected={}",
+                wtr.len(),
+                FILE_HEADER_SIZE,
+            );
+        }
+
+        Ok(wtr)
+    }
+}
+
+#[derive(Debug)]
+pub struct SegmentFile {
+    header: FileHeader,
+    reader: BufReader<File>,
+    writer: BufWriter<File>,
+
+    segment_bytes: usize,
+    segment_seq: usize,
+}
+
+impl SegmentFile {
+    pub fn create<P: AsRef<Path>>(path: P, payload_bytes: u128) -> io::Result<Self> {
+        debug!("create segment file: {:?}", path.as_ref());
+
+        if path.as_ref().exists() {
+            // TODO: add error
+            panic!("already exists: {:?}", path.as_ref());
+        }
+
+        let writer = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path.as_ref())?;
+        let reader = OpenOptions::new().read(true).open(path)?;
+
+        let mut seg = Self {
+            segment_seq: 0,
+
+            header: FileHeader::new(payload_bytes),
+            reader: BufReader::new(reader),
+            writer: BufWriter::new(writer),
+            segment_bytes: payload_bytes as usize + segment::SEGMENT_HEADER_SIZE,
+        };
+        trace!("header of new segment file, header={:?}", seg.header);
+
+        seg.writer.seek(SeekFrom::Start(0))?;
+        seg.writer.write_all(seg.header.to_bytes()?.as_slice())?;
+        seg.reader.seek(SeekFrom::Start(FILE_HEADER_SIZE as u64))?;
+
+        trace!("inited: SegmentFile={:?}", seg);
+        Ok(seg)
+    }
+
+    pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        debug!("open segment file: {:?}", path.as_ref());
+
+        let writer = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(path.as_ref())?;
+        let mut reader = OpenOptions::new().read(true).open(path)?;
+
+        let mut buf: [u8; FILE_HEADER_SIZE] = [0u8; FILE_HEADER_SIZE];
+        let bytes = reader.read(&mut buf)?;
+        if bytes == 0 {
+            // TODO add error
+            panic!("missing header of segment file");
+        };
+
+        let header = FileHeader::from(&buf)?;
+        trace!("file-header={:?}", header);
+        let segment_bytes = (header.header_bytes + header.payload_bytes) as usize;
+
+        let mut seg = Self {
+            segment_seq: 0,
+
+            header,
+            segment_bytes,
+            reader: BufReader::new(reader),
+            writer: BufWriter::new(writer),
+        };
+
+        seg.reader.seek(SeekFrom::Start(FILE_HEADER_SIZE as u64))?;
+        seg.writer.seek(SeekFrom::End(0))?;
+
+        let segments_bytes = seg.writer.seek(SeekFrom::Current(0))? as usize - FILE_HEADER_SIZE;
+        trace!("bytes of segments of file: bytes={}", segments_bytes);
+        if segments_bytes == 0 {
+            seg.segment_seq = 0;
+        } else {
+            assert_eq!(segments_bytes % seg.segment_bytes, 0);
+            seg.segment_seq = (segments_bytes / seg.segment_bytes) - 1;
+        }
+
+        trace!("inited: SegmentFile={:?}", seg);
+        Ok(seg)
+    }
+
+    fn next_segment(&mut self) -> io::Result<Option<Segment>> {
+        debug!("reading next segment");
+        let mut buf: Vec<u8> = vec![0u8; self.segment_bytes];
+        self.reader.read_exact(buf.as_mut_slice())?;
+
+        if buf.is_empty() {
+            trace!("read EOF");
+            Ok(None)
+        } else if buf.len() != self.segment_bytes {
+            panic!(
+                "length of bytes from file mismatch header size: {} vs {}",
+                buf.len(),
+                self.segment_bytes
+            );
+        } else {
+            let segment = Segment::from(buf.as_slice())?;
+            trace!("read next segment success: segment={:?}", segment);
+            Ok(Some(segment))
+        }
+    }
+
+    pub fn seek_segment(&mut self, n: usize) -> io::Result<Option<u64>> {
+        debug!("seeking segment: segment_seq={}", n);
+
+        let bytes = FILE_HEADER_SIZE + self.segment_bytes * n;
+        trace!("seek bytes from start: bytes={}", bytes);
+        if self.reader.seek(SeekFrom::Start(bytes as u64))? as usize == bytes {
+            Ok(Some(bytes as u64))
+        } else {
+            error!("encounter EOF when seeking");
+            Ok(None)
+        }
+    }
+
+    pub fn pop(&mut self) -> io::Result<Option<Vec<u8>>> {
+        debug!("reading full content");
+
+        let mut bytes: Vec<u8> = Vec::new();
+        if let Some(seg) = self.next_segment()? {
+            if seg.is_first() {
+                if seg.is_last() {
+                    trace!(
+                        "read all content success, first is all: content={:?}",
+                        seg.payload()
+                    );
+                    return Ok(Some(seg.payload()));
+                } else {
+                    bytes.extend(seg.payload());
+                }
+            } else {
+                // TODO return error
+                unimplemented!();
+            }
+        } else {
+            return Ok(None);
+        };
+        while let Some(seg) = self.next_segment()? {
+            let is_last = seg.is_last();
+            bytes.extend(seg.payload());
+            if is_last {
+                trace!("read all content success: content={:?}", bytes);
+                return Ok(Some(bytes));
+            }
+        }
+        panic!("incomplete write")
+    }
+
+    pub fn append(&mut self, buf: &[u8]) -> io::Result<usize> {
+        debug!(
+            "writting data into segment_file: data={{len={}, val={:?}}}",
+            buf.len(),
+            buf
+        );
+        let segs = segment::create(buf.to_owned(), self.header.payload_bytes as usize);
+        trace!(
+            "create a segments with data: segments={{len={}, val={:?}}}",
+            segs.len(),
+            segs
+        );
+        let segment_seq = self.segment_seq;
+        for seg in segs {
+            self.writer.write_all(seg.to_bytes()?.as_slice())?;
+            self.segment_seq += 1;
+        }
+
+        trace!(
+            "write all success: segment_file.segment_seq={{before={}, now={}}}",
+            segment_seq,
+            self.segment_seq
+        );
+        Ok(segment_seq)
+    }
+
+    // TODO add `replace`
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, fs};
+
+    use super::*;
+
+    extern crate serde;
+    use serde::{Deserialize, Serialize};
+
+    extern crate serde_json;
+
+    extern crate env_logger;
+    use env_logger::{Builder, Env};
+
+    static INIT: std::sync::Once = std::sync::Once::new();
+    fn init() {
+        INIT.call_once(|| {
+            Builder::from_env(Env::default().default_filter_or("trace"))
+                .is_test(true)
+                .init();
+        })
+    }
+
+    #[test]
+    fn test_file_header() {
+        init();
+        struct Case {
+            header: FileHeader,
+        }
+        let cases = [Case {
+            header: FileHeader::new(128),
+        }];
+
+        for c in cases.iter() {
+            let bytes = c.header.to_bytes().unwrap();
+            assert_eq!(bytes.len(), FILE_HEADER_SIZE);
+            let header = FileHeader::from(bytes.as_slice()).unwrap();
+            assert_eq!(header, c.header);
+            assert_eq!(header.to_bytes().unwrap(), bytes);
+        }
+    }
+
+    #[test]
+    fn test_segment_file() {
+        init();
+        const TEST_PAYLOAD_BYTES: usize = 64;
+
+        #[derive(Debug, PartialEq, Serialize, Deserialize)]
+        struct Case {
+            id: usize,
+            data: Vec<usize>,
+        }
+        let cases = [
+            Case {
+                id: 0,
+                data: vec![0; TEST_PAYLOAD_BYTES / 4],
+            },
+            Case {
+                id: 1,
+                data: vec![1; TEST_PAYLOAD_BYTES / 2],
+            },
+            Case {
+                id: 2,
+                data: vec![2; TEST_PAYLOAD_BYTES],
+            },
+            Case {
+                id: 3,
+                data: vec![3; TEST_PAYLOAD_BYTES * 2],
+            },
+            Case {
+                id: 4,
+                data: vec![4; TEST_PAYLOAD_BYTES * 4],
+            },
+        ];
+        let mut index: HashMap<usize, usize> = HashMap::new();
+
+        let tmp_dir = Path::new("tmp");
+        if tmp_dir.exists() {
+            fs::remove_dir_all(tmp_dir).unwrap();
+        }
+        fs::create_dir(tmp_dir).unwrap();
+
+        {
+            let mut s_file =
+                SegmentFile::create("tmp/tmp.segment", TEST_PAYLOAD_BYTES as u128).unwrap();
+            assert_eq!(s_file.segment_seq, 0);
+            assert_eq!(
+                s_file.segment_bytes,
+                segment::SEGMENT_HEADER_SIZE + TEST_PAYLOAD_BYTES
+            );
+            assert_eq!(
+                s_file.header.header_bytes as usize,
+                segment::SEGMENT_HEADER_SIZE
+            );
+            assert_eq!(s_file.header.payload_bytes as usize, TEST_PAYLOAD_BYTES);
+
+            for (i, case) in cases.iter().enumerate() {
+                let bytes = serde_json::to_vec(case).unwrap();
+                let pos = s_file.append(bytes.as_slice()).unwrap();
+                index.insert(i, pos);
+            }
+            assert_eq!(index.len(), cases.len());
+        }
+
+        let mut s_file = SegmentFile::open("tmp/tmp.segment").unwrap();
+        assert_eq!(
+            s_file.segment_bytes,
+            segment::SEGMENT_HEADER_SIZE + TEST_PAYLOAD_BYTES
+        );
+        assert_eq!(
+            s_file.header.header_bytes as usize,
+            segment::SEGMENT_HEADER_SIZE
+        );
+        assert_eq!(s_file.header.payload_bytes as usize, TEST_PAYLOAD_BYTES);
+
+        for case in &cases {
+            let bytes_json = serde_json::to_vec(case).unwrap();
+            let bytes_seg = s_file.pop().unwrap().unwrap();
+            assert_eq!(bytes_seg, bytes_json);
+
+            let c: Case = serde_json::from_slice(bytes_seg.as_slice()).unwrap();
+            assert_eq!(&c, case);
+        }
+
+        for (i, case) in cases.iter().enumerate() {
+            let seek_bytes = s_file.seek_segment(index[&i]).unwrap().unwrap();
+            assert_eq!(
+                seek_bytes,
+                (FILE_HEADER_SIZE + s_file.segment_bytes * index[&i]) as u64
+            );
+
+            let js_bytes = serde_json::to_vec(case).unwrap();
+            let seg_bytes = s_file.pop().unwrap().unwrap();
+            assert_eq!(seg_bytes, js_bytes);
+
+            let c: Case = serde_json::from_slice(seg_bytes.as_slice()).unwrap();
+            assert_eq!(&c, case);
+        }
+
+        for (i, case) in cases.iter().rev().enumerate() {
+            let seek_bytes = s_file
+                .seek_segment(index[&(cases.len() - i - 1)])
+                .unwrap()
+                .unwrap();
+            assert_eq!(
+                seek_bytes,
+                (FILE_HEADER_SIZE + s_file.segment_bytes * index[&(cases.len() - i - 1)]) as u64
+            );
+            let js_bytes = serde_json::to_vec(case).unwrap();
+            let seg_bytes = s_file.pop().unwrap().unwrap();
+            assert_eq!(seg_bytes, js_bytes);
+
+            let c: Case = serde_json::from_slice(seg_bytes.as_slice()).unwrap();
+            assert_eq!(&c, case);
+        }
+    }
+}
---
+++ rust/projects/lib/segment_io/src/segment.rs
@@ -0,0 +1,283 @@
+use std::{
+    io::{self, Cursor},
+    mem,
+};
+
+use crate::{Endian, ReadBytesExt, WriteBytesExt};
+
+#[derive(Default, Debug, PartialEq)]
+struct SegmentHeader {
+    // TODO add `segment_uuid`
+    length: u128,
+    size: u128,
+    partial_seq: u128,
+    total: u128,
+}
+
+pub(crate) const SEGMENT_HEADER_SIZE: usize = mem::size_of::<SegmentHeader>();
+
+impl SegmentHeader {
+    fn new(length: u128, size: u128, partial_seq: u128, total: u128) -> Self {
+        Self {
+            length,
+            size,
+            partial_seq,
+            total,
+        }
+    }
+
+    fn from(bs: &[u8]) -> io::Result<Self> {
+        assert_eq!(bs.len(), SEGMENT_HEADER_SIZE);
+
+        let mut h = Self::default();
+
+        let mut rdr = Cursor::new(Vec::from(bs));
+
+        h.length = rdr.read_u128::<Endian>()?;
+        h.size = rdr.read_u128::<Endian>()?;
+        h.partial_seq = rdr.read_u128::<Endian>()?;
+        h.total = rdr.read_u128::<Endian>()?;
+
+        Ok(h)
+    }
+
+    fn to_bytes(&self) -> io::Result<Vec<u8>> {
+        let mut wtr = Vec::new();
+
+        wtr.write_u128::<Endian>(self.length)?;
+        wtr.write_u128::<Endian>(self.size)?;
+        wtr.write_u128::<Endian>(self.partial_seq)?;
+        wtr.write_u128::<Endian>(self.total)?;
+
+        if wtr.len() != SEGMENT_HEADER_SIZE {
+            panic!(
+                "length of SegmentHeader's bytes={}, expected={}",
+                wtr.len(),
+                SEGMENT_HEADER_SIZE,
+            );
+        }
+
+        Ok(wtr)
+    }
+}
+
+#[derive(Default, Debug, PartialEq)]
+pub(crate) struct Segment {
+    header: SegmentHeader,
+    payload: Vec<u8>,
+}
+
+impl Segment {
+    fn new(header: SegmentHeader, mut payload: Vec<u8>) -> Self {
+        assert_eq!(payload.len(), header.length as usize);
+        assert!(payload.len() <= header.size as usize);
+
+        if payload.len() < header.size as usize {
+            payload.resize_with(header.size as usize, Default::default);
+        };
+
+        Self { header, payload }
+    }
+
+    pub(crate) fn from(bs: &[u8]) -> io::Result<Self> {
+        assert!(bs.len() >= SEGMENT_HEADER_SIZE);
+
+        let header = SegmentHeader::from(&bs[..SEGMENT_HEADER_SIZE])?;
+        let payload = Vec::from(&bs[SEGMENT_HEADER_SIZE..]);
+
+        Ok(Self { header, payload })
+    }
+
+    pub(crate) fn to_bytes(&self) -> io::Result<Vec<u8>> {
+        let mut bytes = self.header.to_bytes()?;
+
+        bytes.extend(self.payload.clone());
+
+        Ok(bytes)
+    }
+
+    pub(crate) fn payload(&self) -> Vec<u8> {
+        (&self.payload[..self.header.length as usize]).to_owned()
+    }
+
+    pub(crate) fn is_first(&self) -> bool {
+        self.header.partial_seq == 0
+    }
+
+    pub(crate) fn is_last(&self) -> bool {
+        self.header.partial_seq == self.header.total - 1
+    }
+}
+
+pub(crate) fn create(mut payload: Vec<u8>, partial_limits: usize) -> Vec<Segment> {
+    let n = if payload.len() % partial_limits != 0 {
+        payload.len() / partial_limits as usize + 1
+    } else {
+        payload.len() / partial_limits as usize
+    };
+    let mut segments: Vec<Segment> = Vec::with_capacity(n);
+
+    for i in 0..n {
+        let next = if payload.len() >= partial_limits {
+            payload.split_off(partial_limits)
+        } else {
+            vec![]
+        };
+
+        segments.push(Segment::new(
+            SegmentHeader::new(
+                payload.len() as u128,
+                partial_limits as u128,
+                i as u128,
+                n as u128,
+            ),
+            payload,
+        ));
+
+        payload = next;
+    }
+
+    segments
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create() {
+        struct Case {
+            payload: Vec<u8>,
+            partial_limits: usize,
+            expected_segments: usize,
+        }
+        let cases = [
+            Case {
+                payload: vec![1; 1024],
+                partial_limits: 128,
+                expected_segments: 8,
+            },
+            Case {
+                payload: vec![2; 0],
+                partial_limits: 128,
+                expected_segments: 0,
+            },
+            Case {
+                payload: vec![3; 1025],
+                partial_limits: 128,
+                expected_segments: 9,
+            },
+        ];
+
+        for c in &cases {
+            let remain_bytes = c.payload.len() % c.partial_limits;
+            let segments = create(c.payload.clone(), c.partial_limits);
+            assert_eq!(segments.len(), c.expected_segments);
+
+            if c.expected_segments != 0 {
+                assert!(segments[0].is_first());
+
+                let (last_seg, segments) = segments.split_last().unwrap();
+                assert_eq!(last_seg.header.size as usize, c.partial_limits);
+                assert_eq!(last_seg.payload.len(), c.partial_limits);
+                assert!(last_seg.is_last());
+                assert_eq!(last_seg.header.partial_seq as usize, segments.len());
+
+                if remain_bytes != 0 {
+                    assert_eq!(last_seg.header.length as usize, remain_bytes);
+                    assert_eq!(
+                        &last_seg.payload[..remain_bytes],
+                        &c.payload[c.payload.len() - remain_bytes..]
+                    );
+                } else {
+                    assert_eq!(last_seg.header.length as usize, c.partial_limits);
+                    assert_eq!(
+                        last_seg.payload.as_slice(),
+                        &c.payload[c.payload.len() - last_seg.payload.len()..]
+                    );
+                }
+
+                let mut last = 0usize;
+                for (i, seg) in segments.iter().enumerate() {
+                    assert_eq!(seg.header.length as usize, c.partial_limits);
+                    assert_eq!(seg.header.size as usize, c.partial_limits);
+                    assert_eq!(seg.payload.len(), c.partial_limits);
+                    assert_eq!(seg.header.partial_seq as usize, i);
+                    assert_eq!(
+                        seg.payload.as_slice(),
+                        &c.payload[last..last + seg.payload.len()]
+                    );
+                    last += seg.payload.len();
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_header() {
+        struct Case {
+            header: SegmentHeader,
+        }
+        let cases = [Case {
+            header: SegmentHeader {
+                length: 128,
+                size: 128,
+                partial_seq: 8,
+                total: 16,
+            },
+        }];
+
+        for c in cases.iter() {
+            let bytes = c.header.to_bytes().unwrap();
+            assert_eq!(bytes.len(), SEGMENT_HEADER_SIZE);
+            let header = SegmentHeader::from(bytes.as_slice()).unwrap();
+            assert_eq!(header, c.header);
+            assert_eq!(header.to_bytes().unwrap(), bytes);
+        }
+    }
+
+    #[test]
+    fn test_segment() {
+        struct Case {
+            segment: Segment,
+        }
+        let cases = [
+            Case {
+                segment: Segment {
+                    header: SegmentHeader {
+                        length: 128,
+                        size: 128,
+                        partial_seq: 8,
+                        total: 16,
+                    },
+                    payload: vec![0; 128],
+                },
+            },
+            Case {
+                segment: Segment {
+                    header: SegmentHeader {
+                        length: 64,
+                        size: 128,
+                        partial_seq: 8,
+                        total: 16,
+                    },
+                    payload: vec![0; 64],
+                },
+            },
+        ];
+
+        for c in cases.iter() {
+            assert_eq!(
+                c.segment.payload().as_slice(),
+                &c.segment.payload[..c.segment.header.length as usize]
+            );
+
+            let bytes = c.segment.to_bytes().unwrap();
+            assert_eq!(bytes.len(), SEGMENT_HEADER_SIZE + c.segment.payload.len());
+            let segment = Segment::from(bytes.as_slice()).unwrap();
+            assert_eq!(segment, c.segment);
+            assert_eq!(segment.to_bytes().unwrap(), bytes);
+            assert_eq!(segment.payload(), c.segment.payload());
+        }
+    }
+}
