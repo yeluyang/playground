28d5a8f819e293b53324e43fb72d0c93981c394c
yeluyang <ylycpg@gmail.com>
2020-04-13 10:00:45 +0800

    api: add TaskGet

--- dss/projects/map_reduce/src/rpc/rpc.rs
+++ dss/projects/map_reduce/src/rpc/rpc.rs
@@ -266,16 +266,356 @@ impl ::protobuf::reflect::ProtobufValue for PingResponse {
     }
 }
 
+#[derive(PartialEq,Clone,Default)]
+pub struct TaskGetRequest {
+    // message fields
+    pub host: ::std::string::String,
+    // special fields
+    pub unknown_fields: ::protobuf::UnknownFields,
+    pub cached_size: ::protobuf::CachedSize,
+}
+
+impl<'a> ::std::default::Default for &'a TaskGetRequest {
+    fn default() -> &'a TaskGetRequest {
+        <TaskGetRequest as ::protobuf::Message>::default_instance()
+    }
+}
+
+impl TaskGetRequest {
+    pub fn new() -> TaskGetRequest {
+        ::std::default::Default::default()
+    }
+
+    // string host = 1;
+
+
+    pub fn get_host(&self) -> &str {
+        &self.host
+    }
+    pub fn clear_host(&mut self) {
+        self.host.clear();
+    }
+
+    // Param is passed by value, moved
+    pub fn set_host(&mut self, v: ::std::string::String) {
+        self.host = v;
+    }
+
+    // Mutable pointer to the field.
+    // If field is not initialized, it is initialized with default value first.
+    pub fn mut_host(&mut self) -> &mut ::std::string::String {
+        &mut self.host
+    }
+
+    // Take field
+    pub fn take_host(&mut self) -> ::std::string::String {
+        ::std::mem::replace(&mut self.host, ::std::string::String::new())
+    }
+}
+
+impl ::protobuf::Message for TaskGetRequest {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+
+    fn merge_from(&mut self, is: &mut ::protobuf::CodedInputStream<'_>) -> ::protobuf::ProtobufResult<()> {
+        while !is.eof()? {
+            let (field_number, wire_type) = is.read_tag_unpack()?;
+            match field_number {
+                1 => {
+                    ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.host)?;
+                },
+                _ => {
+                    ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
+                },
+            };
+        }
+        ::std::result::Result::Ok(())
+    }
+
+    // Compute sizes of nested messages
+    #[allow(unused_variables)]
+    fn compute_size(&self) -> u32 {
+        let mut my_size = 0;
+        if !self.host.is_empty() {
+            my_size += ::protobuf::rt::string_size(1, &self.host);
+        }
+        my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
+        self.cached_size.set(my_size);
+        my_size
+    }
+
+    fn write_to_with_cached_sizes(&self, os: &mut ::protobuf::CodedOutputStream<'_>) -> ::protobuf::ProtobufResult<()> {
+        if !self.host.is_empty() {
+            os.write_string(1, &self.host)?;
+        }
+        os.write_unknown_fields(self.get_unknown_fields())?;
+        ::std::result::Result::Ok(())
+    }
+
+    fn get_cached_size(&self) -> u32 {
+        self.cached_size.get()
+    }
+
+    fn get_unknown_fields(&self) -> &::protobuf::UnknownFields {
+        &self.unknown_fields
+    }
+
+    fn mut_unknown_fields(&mut self) -> &mut ::protobuf::UnknownFields {
+        &mut self.unknown_fields
+    }
+
+    fn as_any(&self) -> &dyn (::std::any::Any) {
+        self as &dyn (::std::any::Any)
+    }
+    fn as_any_mut(&mut self) -> &mut dyn (::std::any::Any) {
+        self as &mut dyn (::std::any::Any)
+    }
+    fn into_any(self: Box<Self>) -> ::std::boxed::Box<dyn (::std::any::Any)> {
+        self
+    }
+
+    fn descriptor(&self) -> &'static ::protobuf::reflect::MessageDescriptor {
+        Self::descriptor_static()
+    }
+
+    fn new() -> TaskGetRequest {
+        TaskGetRequest::new()
+    }
+
+    fn descriptor_static() -> &'static ::protobuf::reflect::MessageDescriptor {
+        static mut descriptor: ::protobuf::lazy::Lazy<::protobuf::reflect::MessageDescriptor> = ::protobuf::lazy::Lazy::INIT;
+        unsafe {
+            descriptor.get(|| {
+                let mut fields = ::std::vec::Vec::new();
+                fields.push(::protobuf::reflect::accessor::make_simple_field_accessor::<_, ::protobuf::types::ProtobufTypeString>(
+                    "host",
+                    |m: &TaskGetRequest| { &m.host },
+                    |m: &mut TaskGetRequest| { &mut m.host },
+                ));
+                ::protobuf::reflect::MessageDescriptor::new_pb_name::<TaskGetRequest>(
+                    "TaskGetRequest",
+                    fields,
+                    file_descriptor_proto()
+                )
+            })
+        }
+    }
+
+    fn default_instance() -> &'static TaskGetRequest {
+        static mut instance: ::protobuf::lazy::Lazy<TaskGetRequest> = ::protobuf::lazy::Lazy::INIT;
+        unsafe {
+            instance.get(TaskGetRequest::new)
+        }
+    }
+}
+
+impl ::protobuf::Clear for TaskGetRequest {
+    fn clear(&mut self) {
+        self.host.clear();
+        self.unknown_fields.clear();
+    }
+}
+
+impl ::std::fmt::Debug for TaskGetRequest {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        ::protobuf::text_format::fmt(self, f)
+    }
+}
+
+impl ::protobuf::reflect::ProtobufValue for TaskGetRequest {
+    fn as_ref(&self) -> ::protobuf::reflect::ReflectValueRef {
+        ::protobuf::reflect::ReflectValueRef::Message(self)
+    }
+}
+
+#[derive(PartialEq,Clone,Default)]
+pub struct TaskGetResponse {
+    // message fields
+    pub input_path: ::std::string::String,
+    // special fields
+    pub unknown_fields: ::protobuf::UnknownFields,
+    pub cached_size: ::protobuf::CachedSize,
+}
+
+impl<'a> ::std::default::Default for &'a TaskGetResponse {
+    fn default() -> &'a TaskGetResponse {
+        <TaskGetResponse as ::protobuf::Message>::default_instance()
+    }
+}
+
+impl TaskGetResponse {
+    pub fn new() -> TaskGetResponse {
+        ::std::default::Default::default()
+    }
+
+    // string input_path = 1;
+
+
+    pub fn get_input_path(&self) -> &str {
+        &self.input_path
+    }
+    pub fn clear_input_path(&mut self) {
+        self.input_path.clear();
+    }
+
+    // Param is passed by value, moved
+    pub fn set_input_path(&mut self, v: ::std::string::String) {
+        self.input_path = v;
+    }
+
+    // Mutable pointer to the field.
+    // If field is not initialized, it is initialized with default value first.
+    pub fn mut_input_path(&mut self) -> &mut ::std::string::String {
+        &mut self.input_path
+    }
+
+    // Take field
+    pub fn take_input_path(&mut self) -> ::std::string::String {
+        ::std::mem::replace(&mut self.input_path, ::std::string::String::new())
+    }
+}
+
+impl ::protobuf::Message for TaskGetResponse {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+
+    fn merge_from(&mut self, is: &mut ::protobuf::CodedInputStream<'_>) -> ::protobuf::ProtobufResult<()> {
+        while !is.eof()? {
+            let (field_number, wire_type) = is.read_tag_unpack()?;
+            match field_number {
+                1 => {
+                    ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.input_path)?;
+                },
+                _ => {
+                    ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
+                },
+            };
+        }
+        ::std::result::Result::Ok(())
+    }
+
+    // Compute sizes of nested messages
+    #[allow(unused_variables)]
+    fn compute_size(&self) -> u32 {
+        let mut my_size = 0;
+        if !self.input_path.is_empty() {
+            my_size += ::protobuf::rt::string_size(1, &self.input_path);
+        }
+        my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
+        self.cached_size.set(my_size);
+        my_size
+    }
+
+    fn write_to_with_cached_sizes(&self, os: &mut ::protobuf::CodedOutputStream<'_>) -> ::protobuf::ProtobufResult<()> {
+        if !self.input_path.is_empty() {
+            os.write_string(1, &self.input_path)?;
+        }
+        os.write_unknown_fields(self.get_unknown_fields())?;
+        ::std::result::Result::Ok(())
+    }
+
+    fn get_cached_size(&self) -> u32 {
+        self.cached_size.get()
+    }
+
+    fn get_unknown_fields(&self) -> &::protobuf::UnknownFields {
+        &self.unknown_fields
+    }
+
+    fn mut_unknown_fields(&mut self) -> &mut ::protobuf::UnknownFields {
+        &mut self.unknown_fields
+    }
+
+    fn as_any(&self) -> &dyn (::std::any::Any) {
+        self as &dyn (::std::any::Any)
+    }
+    fn as_any_mut(&mut self) -> &mut dyn (::std::any::Any) {
+        self as &mut dyn (::std::any::Any)
+    }
+    fn into_any(self: Box<Self>) -> ::std::boxed::Box<dyn (::std::any::Any)> {
+        self
+    }
+
+    fn descriptor(&self) -> &'static ::protobuf::reflect::MessageDescriptor {
+        Self::descriptor_static()
+    }
+
+    fn new() -> TaskGetResponse {
+        TaskGetResponse::new()
+    }
+
+    fn descriptor_static() -> &'static ::protobuf::reflect::MessageDescriptor {
+        static mut descriptor: ::protobuf::lazy::Lazy<::protobuf::reflect::MessageDescriptor> = ::protobuf::lazy::Lazy::INIT;
+        unsafe {
+            descriptor.get(|| {
+                let mut fields = ::std::vec::Vec::new();
+                fields.push(::protobuf::reflect::accessor::make_simple_field_accessor::<_, ::protobuf::types::ProtobufTypeString>(
+                    "input_path",
+                    |m: &TaskGetResponse| { &m.input_path },
+                    |m: &mut TaskGetResponse| { &mut m.input_path },
+                ));
+                ::protobuf::reflect::MessageDescriptor::new_pb_name::<TaskGetResponse>(
+                    "TaskGetResponse",
+                    fields,
+                    file_descriptor_proto()
+                )
+            })
+        }
+    }
+
+    fn default_instance() -> &'static TaskGetResponse {
+        static mut instance: ::protobuf::lazy::Lazy<TaskGetResponse> = ::protobuf::lazy::Lazy::INIT;
+        unsafe {
+            instance.get(TaskGetResponse::new)
+        }
+    }
+}
+
+impl ::protobuf::Clear for TaskGetResponse {
+    fn clear(&mut self) {
+        self.input_path.clear();
+        self.unknown_fields.clear();
+    }
+}
+
+impl ::std::fmt::Debug for TaskGetResponse {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        ::protobuf::text_format::fmt(self, f)
+    }
+}
+
+impl ::protobuf::reflect::ProtobufValue for TaskGetResponse {
+    fn as_ref(&self) -> ::protobuf::reflect::ReflectValueRef {
+        ::protobuf::reflect::ReflectValueRef::Message(self)
+    }
+}
+
 static file_descriptor_proto_data: &'static [u8] = b"\
-    \n\x13assets/pb/rpc.proto\"\r\n\x0bPingRequest\"\x0e\n\x0cPingResponse2-\
-    \n\x04Node\x12%\n\x04Ping\x12\x0c.PingRequest\x1a\r.PingResponse\"\0J\
-    \x8f\x01\n\x06\x12\x04\0\0\x08\x01\n\x08\n\x01\x0c\x12\x03\0\0\x12\n\t\n\
+    \n\x13assets/pb/rpc.proto\"\r\n\x0bPingRequest\"\x0e\n\x0cPingResponse\"\
+    $\n\x0eTaskGetRequest\x12\x12\n\x04host\x18\x01\x20\x01(\tR\x04host\"0\n\
+    \x0fTaskGetResponse\x12\x1d\n\ninput_path\x18\x01\x20\x01(\tR\tinputPath\
+    2b\n\tMasterRPC\x12%\n\x04Ping\x12\x0c.PingRequest\x1a\r.PingResponse\"\
+    \0\x12.\n\x07TaskGet\x12\x0f.TaskGetRequest\x1a\x10.TaskGetResponse\"\0J\
+    \xe4\x02\n\x06\x12\x04\0\0\x11\x01\n\x08\n\x01\x0c\x12\x03\0\0\x12\n\t\n\
     \x02\x04\0\x12\x03\x02\0\x16\n\n\n\x03\x04\0\x01\x12\x03\x02\x08\x13\n\t\
     \n\x02\x04\x01\x12\x03\x04\0\x17\n\n\n\x03\x04\x01\x01\x12\x03\x04\x08\
-    \x14\n\n\n\x02\x06\0\x12\x04\x06\0\x08\x01\n\n\n\x03\x06\0\x01\x12\x03\
-    \x06\x08\x0c\n\x0b\n\x04\x06\0\x02\0\x12\x03\x07\x043\n\x0c\n\x05\x06\0\
-    \x02\0\x01\x12\x03\x07\x08\x0c\n\x0c\n\x05\x06\0\x02\0\x02\x12\x03\x07\r\
-    \x18\n\x0c\n\x05\x06\0\x02\0\x03\x12\x03\x07#/b\x06proto3\
+    \x14\n\n\n\x02\x04\x02\x12\x04\x06\0\x08\x01\n\n\n\x03\x04\x02\x01\x12\
+    \x03\x06\x08\x16\n\x0b\n\x04\x04\x02\x02\0\x12\x03\x07\x04\x14\n\x0c\n\
+    \x05\x04\x02\x02\0\x05\x12\x03\x07\x04\n\n\x0c\n\x05\x04\x02\x02\0\x01\
+    \x12\x03\x07\x0b\x0f\n\x0c\n\x05\x04\x02\x02\0\x03\x12\x03\x07\x12\x13\n\
+    \n\n\x02\x04\x03\x12\x04\n\0\x0c\x01\n\n\n\x03\x04\x03\x01\x12\x03\n\x08\
+    \x17\n\x0b\n\x04\x04\x03\x02\0\x12\x03\x0b\x04\x1a\n\x0c\n\x05\x04\x03\
+    \x02\0\x05\x12\x03\x0b\x04\n\n\x0c\n\x05\x04\x03\x02\0\x01\x12\x03\x0b\
+    \x0b\x15\n\x0c\n\x05\x04\x03\x02\0\x03\x12\x03\x0b\x18\x19\n\n\n\x02\x06\
+    \0\x12\x04\x0e\0\x11\x01\n\n\n\x03\x06\0\x01\x12\x03\x0e\x08\x11\n\x0b\n\
+    \x04\x06\0\x02\0\x12\x03\x0f\x043\n\x0c\n\x05\x06\0\x02\0\x01\x12\x03\
+    \x0f\x08\x0c\n\x0c\n\x05\x06\0\x02\0\x02\x12\x03\x0f\r\x18\n\x0c\n\x05\
+    \x06\0\x02\0\x03\x12\x03\x0f#/\n\x0b\n\x04\x06\0\x02\x01\x12\x03\x10\x04\
+    <\n\x0c\n\x05\x06\0\x02\x01\x01\x12\x03\x10\x08\x0f\n\x0c\n\x05\x06\0\
+    \x02\x01\x02\x12\x03\x10\x10\x1e\n\x0c\n\x05\x06\0\x02\x01\x03\x12\x03\
+    \x10)8b\x06proto3\
 ";
 
 static mut file_descriptor_proto_lazy: ::protobuf::lazy::Lazy<::protobuf::descriptor::FileDescriptorProto> = ::protobuf::lazy::Lazy::INIT;
--- dss/projects/map_reduce/src/rpc/rpc_grpc.rs
+++ dss/projects/map_reduce/src/rpc/rpc_grpc.rs
@@ -18,27 +18,34 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 
-const METHOD_NODE_PING: ::grpcio::Method<super::rpc::PingRequest, super::rpc::PingResponse> = ::grpcio::Method {
+const METHOD_MASTER_RPC_PING: ::grpcio::Method<super::rpc::PingRequest, super::rpc::PingResponse> = ::grpcio::Method {
     ty: ::grpcio::MethodType::Unary,
-    name: "/Node/Ping",
+    name: "/MasterRPC/Ping",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
+const METHOD_MASTER_RPC_TASK_GET: ::grpcio::Method<super::rpc::TaskGetRequest, super::rpc::TaskGetResponse> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/MasterRPC/TaskGet",
     req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
     resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
 };
 
 #[derive(Clone)]
-pub struct NodeClient {
+pub struct MasterRpcClient {
     client: ::grpcio::Client,
 }
 
-impl NodeClient {
+impl MasterRpcClient {
     pub fn new(channel: ::grpcio::Channel) -> Self {
-        NodeClient {
+        MasterRpcClient {
             client: ::grpcio::Client::new(channel),
         }
     }
 
     pub fn ping_opt(&self, req: &super::rpc::PingRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::rpc::PingResponse> {
-        self.client.unary_call(&METHOD_NODE_PING, req, opt)
+        self.client.unary_call(&METHOD_MASTER_RPC_PING, req, opt)
     }
 
     pub fn ping(&self, req: &super::rpc::PingRequest) -> ::grpcio::Result<super::rpc::PingResponse> {
@@ -46,26 +53,47 @@ impl NodeClient {
     }
 
     pub fn ping_async_opt(&self, req: &super::rpc::PingRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::rpc::PingResponse>> {
-        self.client.unary_call_async(&METHOD_NODE_PING, req, opt)
+        self.client.unary_call_async(&METHOD_MASTER_RPC_PING, req, opt)
     }
 
     pub fn ping_async(&self, req: &super::rpc::PingRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::rpc::PingResponse>> {
         self.ping_async_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn task_get_opt(&self, req: &super::rpc::TaskGetRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::rpc::TaskGetResponse> {
+        self.client.unary_call(&METHOD_MASTER_RPC_TASK_GET, req, opt)
+    }
+
+    pub fn task_get(&self, req: &super::rpc::TaskGetRequest) -> ::grpcio::Result<super::rpc::TaskGetResponse> {
+        self.task_get_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn task_get_async_opt(&self, req: &super::rpc::TaskGetRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::rpc::TaskGetResponse>> {
+        self.client.unary_call_async(&METHOD_MASTER_RPC_TASK_GET, req, opt)
+    }
+
+    pub fn task_get_async(&self, req: &super::rpc::TaskGetRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::rpc::TaskGetResponse>> {
+        self.task_get_async_opt(req, ::grpcio::CallOption::default())
     }
     pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Item = (), Error = ()> + Send + 'static {
         self.client.spawn(f)
     }
 }
 
-pub trait Node {
+pub trait MasterRpc {
     fn ping(&mut self, ctx: ::grpcio::RpcContext, req: super::rpc::PingRequest, sink: ::grpcio::UnarySink<super::rpc::PingResponse>);
+    fn task_get(&mut self, ctx: ::grpcio::RpcContext, req: super::rpc::TaskGetRequest, sink: ::grpcio::UnarySink<super::rpc::TaskGetResponse>);
 }
 
-pub fn create_node<S: Node + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
+pub fn create_master_rpc<S: MasterRpc + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
     let mut builder = ::grpcio::ServiceBuilder::new();
-    let mut instance = s;
-    builder = builder.add_unary_handler(&METHOD_NODE_PING, move |ctx, req, resp| {
+    let mut instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_MASTER_RPC_PING, move |ctx, req, resp| {
         instance.ping(ctx, req, resp)
+    });
+    let mut instance = s;
+    builder = builder.add_unary_handler(&METHOD_MASTER_RPC_TASK_GET, move |ctx, req, resp| {
+        instance.task_get(ctx, req, resp)
     });
     builder.build()
 }
