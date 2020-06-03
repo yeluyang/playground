dc83cd826346bd24f8921b24aa881ba79d6f1a6d
yeluyang <ylycpg@gmail.com>
2020-06-03 20:21:08 +0800

    test[dss/raft]: update

--- dss/projects/raft/tests/tests.rs
+++ dss/projects/raft/tests/tests.rs
@@ -1,18 +1,37 @@
-use std::fs;
+use std::{fs, thread};
 
 use raft::*;
 
 #[test]
 fn all() {
-    let config = Config {
-        ip: "127.0.0.1".to_owned(),
-        port: 10086,
-        logs: "tmp/tests/all/logs".to_owned(),
-        peers: vec![("127.0.0.1".to_owned(), 10087)],
-    };
+    let configs = [
+        Config {
+            ip: "127.0.0.1".to_owned(),
+            port: 10081,
+            logs: "tmp/tests/all/1/logs".to_owned(),
+            peers: vec![("127.0.0.1".to_owned(), 10087)],
+        },
+        Config {
+            ip: "127.0.0.1".to_owned(),
+            port: 10082,
+            logs: "tmp/tests/all/2/logs".to_owned(),
+            peers: vec![("127.0.0.1".to_owned(), 10087)],
+        },
+        Config {
+            ip: "127.0.0.1".to_owned(),
+            port: 10083,
+            logs: "tmp/tests/all/3/logs".to_owned(),
+            peers: vec![("127.0.0.1".to_owned(), 10087)],
+        },
+    ];
+    let mut servers = Vec::new();
 
-    fs::create_dir_all(&config.logs).unwrap();
+    for config in &configs {
+        fs::create_dir_all(&config.logs).unwrap();
+        servers.push(PeerServer::new(config.clone()));
+    }
 
-    let mut server = PeerServer::new(config.clone());
-    server.run()
+    for mut server in servers {
+        thread::spawn(move || server.run());
+    }
 }
