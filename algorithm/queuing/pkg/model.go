bb285ebb2cf80719e6d137e09f7756becbca5b60
yeluyang <ylycpg@gmail.com>
2020-08-20 14:12:32 +0800

    refactor[algo/queuing]: merge processor into model

--- algorithm/queuing/pkg/model.go
+++ algorithm/queuing/pkg/model.go
@@ -2,6 +2,9 @@ package pkg
 
 import (
 	"math"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type ProcessorState struct {
@@ -22,4 +25,36 @@ func NewProcessorState(tps float64, qps float64, peers int) *ProcessorState {
 		RemainCeil:  int(math.Max(incomeCeil-tps, 0)),
 		RemainFloor: int(math.Max(incomeFloor-tps, 0)),
 	}
+}
+
+type Processor struct {
+	ID    string
+	Cost  time.Duration
+	TPS   float64
+	peers int
+	State *ProcessorState
+}
+
+func NewProcessor(cost time.Duration, peers int) *Processor {
+	return &Processor{
+		ID:    uuid.NewV4().String(),
+		Cost:  cost,
+		TPS:   float64(time.Second) / float64(cost),
+		peers: peers,
+		State: nil,
+	}
+}
+
+func (p *Processor) Calc(qps float64) {
+	p.State = NewProcessorState(p.TPS, qps, p.peers)
+}
+
+func NewProcessorSet(costs []time.Duration) []*Processor {
+	processors := make([]*Processor, len(costs))
+
+	for i := range costs {
+		processors[i] = NewProcessor(costs[i], len(costs))
+	}
+
+	return processors
 }
