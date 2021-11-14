974910da2394ecd9305cbc5f28eda379ff2fc203
yeluyang <ylycpg@gmail.com>
2021-11-14 12:57:17 +0800

    code[algo/lc]: add questions from history

---
+++ algorithm/leetcode/questions/accepted/21.合并两个有序链表.rs
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=rust
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+    }
+}
+// @lc code=end
+
