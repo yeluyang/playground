4c32be28f2c860af0c71e50cf918fc9a087bc82e
yeluyang <ylycpg@gmail.com>
2021-11-24 18:31:47 +0800

    code[algo/lc]: add questions from history

---
+++ algorithm/leetcode/questions/wrong/334.递增的三元子序列.rs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=334 lang=rust
+ *
+ * [334] 递增的三元子序列
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn increasing_triplet(nums: Vec<i32>) -> bool {}
+}
+// @lc code=end
