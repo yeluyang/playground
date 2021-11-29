66fe67a6493d65dd2f7752e8337f4458e5c4f119
yeluyang <ylycpg@gmail.com>
2021-11-29 13:24:33 +0800

    code[algo/lc]: add questions from history

---
+++ algorithm/leetcode/questions/wrong/230.二叉搜索树中第k小的元素.rs
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=rust
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+
+    }
+}
+// @lc code=end
+
