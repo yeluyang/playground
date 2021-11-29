4224f882d938defbf52e078847357d8bd9beef3a
yeluyang <ylycpg@gmail.com>
2021-11-29 22:29:31 +0800

    code[algo/lc]: add questions from history

---
+++ algorithm/leetcode/questions/wrong/347.前-k-个高频元素.rs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=rust
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {}
+}
+// @lc code=end
